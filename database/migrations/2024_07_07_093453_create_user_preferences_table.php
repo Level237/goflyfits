061488ee746d2c94faa5d84b1f428f233f1ba9f1
level237 <Bramslevel129@gmail.com>
2024-07-08 12:14:24 +0100

    seeder role and user

--- database/migrations/2024_07_07_093453_create_user_preferences_table.php
+++ database/migrations/2024_07_07_093453_create_user_preferences_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,16 +13,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('clothing', function (Blueprint $table) {
+        Schema::create('user_preferences', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->string('price');
-            $table->string('size');
+            $table->foreignIdFor(User::class)
+            ->constrained()
+            ->restrictOnDelete()
+            ->restrictOnUpdate();
             $table->foreignIdFor(Category::class)
             ->constrained()
-            ->restrictOnUpdate()
-            ->restrictOnDelete();
+            ->restrictOnDelete()
+            ->restrictOnUpdate();
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('clothing');
+        Schema::dropIfExists('user_preferences');
     }
 };
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'id'=>1,
+            'name'=>"Admin",
+            'email'=>"admin@goflyfits.com",
+            'role_id'=>1,
+            'password'=>bcrypt('password'),
+            'town'=>"douala",
+            'country'=>"cameroon",
+            'phone_number'=>"690394365",
+            'isChoice'=>1
+        ],
+        );
+    }
+}
