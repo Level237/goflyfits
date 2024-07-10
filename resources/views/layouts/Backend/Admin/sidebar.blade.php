ef6b1b81aa7ac62e444261a58ddbee08d7cf7f26
level237 <Bramslevel129@gmail.com>
2024-07-10 11:41:36 +0100

    update sidebar admin

--- resources/views/layouts/Backend/Admin/sidebar.blade.php
+++ resources/views/layouts/Backend/Admin/sidebar.blade.php
@@ -14,7 +14,7 @@
             <!-- Sidebar menu START -->
             <ul class="navbar-nav flex-column" id="navbar-sidebar">
                 <!-- Menu item -->
-                <li class="nav-item"><a href="admin-dashboard.html.htm" class="nav-link active">Dashboard</a></li>
+                <li class="nav-item"><a href="{{ route('admin.dashboard') }}" class="nav-link active">Dashboard</a></li>
 
                 <!-- Title -->
                 <li class="nav-item ms-2 my-2">Pages</li>
@@ -22,70 +22,38 @@
                 <!-- Menu item -->
                 <li class="nav-item">
                     <a class="nav-link" data-bs-toggle="collapse" href="#collapsebooking" role="button" aria-expanded="false" aria-controls="collapsebooking">
-                    Bookings
+                   Vetements
                     </a>
                     <!-- Submenu -->
                     <ul class="nav collapse flex-column" id="collapsebooking" data-bs-parent="#navbar-sidebar">
-                        <li class="nav-item"> <a class="nav-link" href="admin-booking-list.html.htm">Booking List</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="admin-booking-detail.html.htm">Booking Detail</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="admin-booking-list.html.htm">Créer un look</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="admin-booking-detail.html.htm">Listes des look</a></li>
                     </ul>
                 </li>
 
                 <!-- Menu item -->
                 <li class="nav-item">
                     <a class="nav-link" data-bs-toggle="collapse" href="#collapseguest" role="button" aria-expanded="false" aria-controls="collapseguest">
-                    Guests
+                    Categorie
                     </a>
                     <!-- Submenu -->
                     <ul class="nav collapse flex-column" id="collapseguest" data-bs-parent="#navbar-sidebar">
-                        <li class="nav-item"> <a class="nav-link" href="admin-guest-list.html">Guest List</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="admin-guest-detail.html.htm">Guest Detail</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="admin-guest-list.html">Créer une categorie</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="admin-guest-detail.html.htm">Listes des categories</a></li>
                     </ul>
                 </li>
 
                 <!-- Menu item -->
                 <li class="nav-item">
                     <a class="nav-link" data-bs-toggle="collapse" href="#collapseagent" role="button" aria-expanded="false" aria-controls="collapseagent">
-                    Agents
+                    Utilisateurs
                     </a>
                     <!-- Submenu -->
                     <ul class="nav collapse flex-column" id="collapseagent" data-bs-parent="#navbar-sidebar">
-                        <li class="nav-item"> <a class="nav-link" href="admin-agent-list.html">Agent List</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="admin-agent-detail.html">Agent Detail</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="admin-agent-list.html">Listes des utilisateurs</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="admin-agent-detail.html">Créer un utilisateur</a></li>
                     </ul>
                 </li>
-
-                <!-- Menu item -->
-                <li class="nav-item"> <a class="nav-link" href="admin-reviews.html">Reviews</a></li>
-
-                <!-- Menu item -->
-                <li class="nav-item"> <a class="nav-link" href="admin-earnings.html">Earnings</a></li>
-
-                <!-- Menu item -->
-                <li class="nav-item"> <a class="nav-link" href="admin-settings.html.htm">Admin Settings</a></li>
-
-                <!-- Menu item -->
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="collapse" href="#collapseauthentication" role="button" aria-expanded="false" aria-controls="collapseauthentication">
-                        Authentication
-                    </a>
-                    <!-- Submenu -->
-                    <ul class="nav collapse flex-column" id="collapseauthentication" data-bs-parent="#navbar-sidebar">
-                        <li class="nav-item"> <a class="nav-link" href="sign-up.html.htm">Sign Up</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="sign-in.html.htm">Sign In</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="forgot-password.html.htm">Forgot Password</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="error.html.htm">Error 404</a></li>
-                    </ul>
-                </li>
-
-                <!-- Title -->
-                <li class="nav-item ms-2 my-2">Documentation</li>
-
-                <!-- Menu item -->
-                <li class="nav-item"> <a class="nav-link" href="docs/index.html.htm">Documentation</a></li>
-
-                <!-- Menu item -->
-                <li class="nav-item"> <a class="nav-link" href="docs/changelog.html.htm">Changelog</a></li>
             </ul>
             <!-- Sidebar menu end -->
 
