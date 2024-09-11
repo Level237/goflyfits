27f53fc981914837a27006934e333020a04b36a9
level237 <Bramslevel129@gmail.com>
2024-09-11 12:26:17 +0100

    update

--- app/Models/Measure_men.php
+++ app/Models/Measure_men.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Measure_men extends Model
 {
     use HasFactory;
+
+    public function mesures():MorphMany
+    {
+        return $this->morphMany(Measure::class,"measurable");
+    }
 }
